6f5eb4c14458751ce2de98d44eff4cb54664607b
Omonijo Emmanuel Olukayode <eomonijo877@gmail.com>
2023-11-21 15:17:28 +0100

    Add project files.

---
+++ Area of a Triangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TriangleAreaCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Triangle Area Calculator");
+
+            // Get the base and height from the user
+            Console.Write("Enter the base of the triangle: ");
+            double baseLength = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the height of the triangle: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            // Calculate the area of the triangle
+            double area = CalculateTriangleArea(baseLength, height);
+
+            // Display the result
+            Console.WriteLine($"The area of the triangle is: {area}");
+        }
+
+        // Function to calculate the area of a triangle
+        static double CalculateTriangleArea(double baseLength, double height)
+        {
+            return (baseLength * height) / 2;
+        }
+    }
+}
